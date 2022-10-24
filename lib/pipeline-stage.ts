295e88576d00db9c1bf9fe111231364f3f85c286
Shariful Islam <shariful.islam.mec@gmail.com>
2022-10-24 20:54:00 +0300

    endpoint_url added

--- lib/pipeline-stage.ts
+++ lib/pipeline-stage.ts
@@ -1,11 +1,17 @@
 import { CdkWorkshop1Stack } from './cdk-workshop1-stack';
-import { Stage, StageProps } from 'aws-cdk-lib';
+import { CfnOutput, Stage, StageProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 export class Workshop1PipelineStage extends Stage {
+    public readonly hcViewerUrl: CfnOutput;
+    public readonly hcEndpoint: CfnOutput;
+    
     constructor(scope: Construct, id: string, props?: StageProps) {
         super(scope, id, props);
 
-        new CdkWorkshop1Stack(this, 'WebService');
+        const service = new CdkWorkshop1Stack(this, 'WebService');
+
+        this.hcEndpoint = service.hcEndpoint;
+        this.hcViewerUrl = service.hcViewerUrl;
     }
 }
